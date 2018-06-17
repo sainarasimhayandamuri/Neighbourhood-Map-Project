2c89e8c34312ed4128d7e297f1179d379eaf08ad
SAI NARASIMHA YANDAMURI <38848935+sainarasimhayandamuri@users.noreply.github.com>
2018-06-17 23:37:26 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,263 @@
+//Array of initial 5 locations that are set by default
+var initialLocations = [{
+	title: 'M. Chinnaswamy Stadium',
+	location: {
+		lat: 12.9788,
+		lng: 77.5996
+	}
+}, {
+	title: 'M.A. Chidambaram Stadium',
+	location: {
+		lat: 13.0629,
+		lng: 80.2792
+	}
+}, {
+	title: 'Rajiv Gandhi Stadium',
+	location: {
+		lat: 17.4065,
+		lng: 78.5505
+	}
+}, {
+	title: 'Wankhede Stadium',
+	location: {
+		lat: 18.9389,
+		lng: 72.8258
+	}
+}];
+
+// Each location on the List has two main properties that can be accessed -
+// title and location
+var Location = function(data) {
+	this.title = data.title;
+	this.location = data.location;
+};
+
+//Where all the controlling takes palce
+var ViewModel = function() {
+	var self = this;
+
+	//an array to store all the locations in
+	this.locationList = ko.observableArray(
+		[]);
+
+	//the list that will appear when being filtered by a keyword
+	this.filter = ko.observable();
+
+	//looping through each item in initialLocations and
+	//adding it to the array
+	initialLocations.forEach(function(
+		locationItem) {
+		self.locationList.push(new Location(
+			locationItem));
+	});
+
+	var largeInfoWindow = new google.maps
+		.InfoWindow();
+	var bounds = new google.maps.LatLngBounds();
+
+	self.locationList().forEach(function(
+		location) {
+		// define the marker
+		var marker = new google.maps.Marker({
+			map: map,
+			position: location.location,
+			title: location.title,
+			animation: google.maps.Animation
+				.DROP
+		});
+
+		location.marker = marker;
+
+
+		//onclick event to open infoWindow
+		//updated to also toggleBounce
+		//Atribution: Udacity's Google Maps APIS Course
+		location.marker.addListener(
+			'click',
+			function() {
+				populateInfoWindow(this,
+					largeInfoWindow);
+				toggleBounce(this);
+			});
+
+		bounds.extend(location.marker.position);
+
+	});
+
+	map.fitBounds(bounds);
+
+
+	// Attribution for filter: http://www.knockmeout.net/2011/04/utility-functions-in-knockoutjs.html
+	// Idea on how to organize it came from this blog with some modifications eg. display/hide markers
+	//It takes in a filter input
+	//if not input is given, displays all the locations and all the markers are set on the map
+	//if an input is given, it compares the input with the title of each location in locationList
+	//and also sets them on the map if they are true - this is after being placed in the locationList
+	//updated using indexOf rather than startsWith to make the filter less strict
+	this.filteredLocations = ko.computed(
+		function() {
+			var filter = self.filter();
+			if (!self.filter()) {
+				self.locationList().forEach(
+					function(location) {
+						location.marker.setMap(map);
+					});
+				return self.locationList();
+			} else {
+				return ko.utils.arrayFilter(self.locationList(),
+					function(loc) {
+						if (loc.title.toLowerCase().indexOf(
+								filter.toLowerCase()) !== -1) {
+							loc.marker.setMap(map);
+						} else {
+							loc.marker.setMap(null);
+						}
+						return loc.title.toLowerCase()
+							.indexOf(filter.toLowerCase()) !==
+							-1;
+					});
+			}
+		}, self);
+
+	//bounce when location is clicked
+	function toggleBounce(marker) {
+		if (marker.getAnimation() !== null) {
+			marker.setAnimation(null);
+		} else {
+			for (var i = 0; i < self.locationList()
+				.length; i++) {
+				var mark = self.locationList()[i].marker;
+				if (mark.getAnimation() !== null) {
+					mark.setAnimation(null);
+				}
+			}
+			marker.setAnimation(google.maps.Animation
+				.BOUNCE);
+		}
+	}
+
+	//initially sets the current location to the first item in
+	//locationList
+	this.currentLocation = ko.observable(
+		this.locationList()[0]);
+
+
+	//this is where the location is set once it has been clicked on
+	//it also makes the marker bounce and infoWindow open when selected
+	//from the list
+	this.setLocation = function(
+		clickedLocation) {
+		toggleBounce(clickedLocation.marker);
+		populateInfoWindow(clickedLocation.marker,
+			largeInfoWindow);
+		self.currentLocation(
+			clickedLocation);
+	};
+};
+
+// -------------------------------------------------------------------------------------
+var map;
+
+function initMap() {
+	// Constructor creates a new map - only center and zoom are required.
+	map = new google.maps.Map(document.getElementById(
+		'map'), {
+		center: {
+			lat: 17.4065,
+			lng: 78.5505
+		},
+		zoom: 13
+	});
+
+	//Attribution: http://stackoverflow.com/questions/8792676/center-google-maps-v3-on-browser-resize-responsive
+	//I used this to keep map centered during resizing window
+	var center;
+
+	function calculateCenter() {
+		center = map.getCenter();
+	}
+	google.maps.event.addDomListener(map,
+		'idle',
+		function() {
+			calculateCenter();
+		});
+	google.maps.event.addDomListener(
+		window, 'resize',
+		function() {
+			map.setCenter(center);
+		});
+
+	ko.applyBindings(new ViewModel());
+}
+
+//error handling function that gets called when google maps api does not return successfully
+function googleErrorHandler() {
+	$('#map-error').html(
+		'<h2>Failed to retrieve Google Maps resources, please try again later.</h2>'
+	);
+}
+
+//this populates the infoWindow for each marker and also calls the wikipedia api to add more info
+//to the infoWindow as well as the title of the location
+function populateInfoWindow(marker,
+	infowindow) {
+	if (infowindow.marker != marker) {
+		infowindow.marker = marker;
+
+		//Wikipedia API - code idea taken from work done in the Intro to AJAX course
+		var wikiUrl =
+			'https://en.wikipedia.org/w/api.php?action=opensearch&search=' +
+			marker.title +
+			'&format=json&callback=wikiCallback';
+
+		var wikiRequestTimeout = setTimeout(
+			function() {
+				infowindow.setContent(
+					"failed to get wikipedia resources"
+				);
+			}, 800);
+
+		$.ajax({
+			url: wikiUrl,
+			dataType: 'jsonp'
+		}).done(function(response) {
+			var articleList = response[1];
+			for (var i = 0; i < articleList.length; i++) {
+				var articleStr = articleList[i];
+				var url =
+					'https://www.wikipedia.org/wiki/' +
+					articleStr;
+				infowindow.setContent('<div><h3>' +
+					marker.title +
+					'</h3><p>Wiki Info: <a href="' +
+					url + '">' + articleStr +
+					'</a></p></div>');
+			}
+
+			clearTimeout(wikiRequestTimeout);
+		});
+
+		infowindow.open(map, marker);
+
+		infowindow.addListener('closeclick',
+			function() {
+				infowindow.close();
+				marker.setAnimation(null);
+			});
+	}
+}
+//hamburger menu functionality from udacity's
+//responsive web design fundamentals
+//updated to use knockout click binding
+
+var main = document.querySelector(
+	'.main');
+var drawer = document.querySelector(
+	'#drawer');
+
+//when the menu icon is clicked, the filter menu slides in
+//and the map/menu shift to the right
+this.openMenu = function() {
+	drawer.classList.toggle('open');
+	main.classList.toggle('moveRight');
+};
